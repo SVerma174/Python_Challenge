22e3f2ec4551d5e36e6c7d35e2968a5a408a6177
Sverma96 <126442928+SVerma174@users.noreply.github.com>
2025-03-11 12:42:24 +0530

    litilt bit update

--- 100 Day Challenage/Day15/start15.py
+++ 100 Day Challenage/Day15/start15.py
@@ -1,13 +1,36 @@
-"""  Exceivce 
+"""  Home Work
 create a program for Good moring sir accoding to current time 
-
-"""
-
-# 
+# with import 
 
 import time
 
-timestamp=time.strftime("%H:%M:%S")
-print(timestamp)
+hours =time.strftime("%H:%M:%S")
+print(hours)
 
-if timestamp
+hours = int(time.strftime("%H"))
+print(hours)
+
+if 5 <= hours < 12:
+    print("Good morning, sir!")
+elif 12 <= hours < 17:
+    print("Good afternoon, sir!")
+elif 17 <= hours < 21:
+    print("Good evening, sir!")
+else:
+    print("Good night, sir!")
+
+
+    """
+
+# Without Any import libray basic program 
+
+Time=int(input("Enter the current time : "))
+
+if 5<= Time < 12:
+    print("Good morning sir !")
+elif 12<= Time < 17:
+  print("Good Afternoon sir !")
+elif 17 <= Time <21:
+    print("Good Evening sir !")
+else:
+    print("Good night sir !")
